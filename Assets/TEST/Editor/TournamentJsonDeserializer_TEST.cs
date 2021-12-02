b5b1e57f3e8cdad59ab8c520f53cdc51fca946a4
Alan Schumann <elgodox@gmail.com>
2021-12-02 11:17:20 -0300

    Test Json Deserializer

---
+++ Assets/TEST/Editor/TournamentJsonDeserializer_TEST.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TEST.Editor
+{
+    public class TournamentJsonDeserializer_TEST
+    {
+        [Test]
+        public void Test_Deserializer()
+        {
+            string json = @"{
+                              'data': [
+                                {
+                                  'type': 'tournament',
+                                  'id': 'eu-fls7',
+                                  'attributes': {
+                                    'createdAt': '2021-11-28T12:46:24Z'
+                                  }
+                                },
+                                {
+                                  'type': 'tournament',
+                                  'id': 'as-pgcwf1',
+                                  'attributes': {
+                                    'createdAt': '2021-11-27T10:38:04Z'
+                                  }
+                                }
+	                            ],
+	                            'links': {
+                                'self': 'https://api.pubg.com/tournaments'
+                              },
+                              'meta': {}
+                            }";
+
+            var deserializer = new TournamentJsonDeserializer();
+            var result = deserializer.Deserialize(json);
+
+            DateTime dateTimeFirst = new DateTime(2021,11,28,12,46,24);
+            DateTime dateTimeSecond = new DateTime(2021,11,27,10,38,04);
+            
+            Assert.AreEqual("eu-fls7",result.data[0].id);
+            Assert.AreEqual(dateTimeFirst,result.data[0].attributes.createdAt);
+            
+            Assert.AreEqual("as-pgcwf1",result.data[1].id);
+            Assert.AreEqual(dateTimeSecond,result.data[1].attributes.createdAt);
+             
+            
+            
+        }
+    }
+}
